bd7db9b47f1c7be42499a82578848f45b4ebfa2c
Hadis Sulejmani <hadissulejmani@outlook.com>
2022-07-17 12:28:43 +0200

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import "./App.css";
+import { BrowserRouter, Route } from "react/router";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <h1>My Articles</h1>
-      </nav>
+      <BrowserRouter>
+        <nav>
+          <h1>My Articles</h1>
+        </nav>
+
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </BrowserRouter>
     </div>
   );
 }
